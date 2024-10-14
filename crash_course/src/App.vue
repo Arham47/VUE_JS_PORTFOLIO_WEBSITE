bb0dd9eb8d4bb09f3865e3d2ad386a507fe8d2bd
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-14 21:31:04 +0500

    vue crash course

---
+++ crash_course/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+
+<template>
+  <Navbar />
+ 
+  <HomeCards/>
+<h1 class="text-2xl font-bold">hello</h1>
+</template>
